23e0785dc3e2ee780d458ba743372cafbf7de096
CusMeDroid <108187260+CusMeDroid@users.noreply.github.com>
2024-04-11 19:12:00 +0700

    Add files via upload

--- assets/js/copyright/index.js
+++ assets/js/copyright/index.js
@@ -18,6 +18,15 @@ document.addEventListener("keydown", (e) => {
   e.ctrlKey && "p" == e.key && (alert("This section is not allowed to print or export to PDF"), (e.cancelBubble = !0), e.preventDefault(), e.stopImmediatePropagation());
 });
 
+const myurl = "cusmedroid.github.io";
+
 const d = new Date();
 let year = d.getFullYear();
 document.getElementById("cpr").innerHTML = year;
+
+if (window.location.hostname == myurl) {
+  // alert("Sukses");
+} else {
+  alert("Buy source");
+  window.location.href = "https://api.whatsapp.com/send?phone=6285772757932&text=Hello,%20I%20Need%20Source%20Code%20Hot%20News%20Template.";
+}
