4dc02336a7d6b87a72bb75acaf2fe8acd98d16d4
CusMeDroid <108187260+CusMeDroid@users.noreply.github.com>
2024-04-13 10:05:37 +0700

    Add files via upload

---
+++ assets/js/contact/index.js
@@ -0,0 +1,11 @@
+const mUsername = document.getElementById("username").value;
+const mEmail = document.getElementById("email").value;
+const mSubject = document.getElementById("subject").value;
+
+function mContact() {
+  if (mUsername < 1 || mEmail < 1 || mSubject < 1) {
+    alert("Please do not ne empty!");
+  } else {
+    window.open('mailto:cusmedroidteam@gmail.com?subject='+mUsername+', '+mEmail+'&body='+mSubject);
+  }
+}
