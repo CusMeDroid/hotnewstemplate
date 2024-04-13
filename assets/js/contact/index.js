4b8bbf8bac842ec92c75627543b959cf4f6e9ade
CusMeDroid <108187260+CusMeDroid@users.noreply.github.com>
2024-04-13 10:09:39 +0700

    Update index.js

--- assets/js/contact/index.js
+++ assets/js/contact/index.js
@@ -1,8 +1,7 @@
-const mUsername = document.getElementById("username").value;
-const mEmail = document.getElementById("email").value;
-const mSubject = document.getElementById("subject").value;
-
 function mContact() {
+  const mUsername = document.getElementById("username").value;
+  const mEmail = document.getElementById("email").value;
+  const mSubject = document.getElementById("subject").value;
   if (mUsername < 1 || mEmail < 1 || mSubject < 1) {
     alert("Please do not ne empty!");
   } else {
